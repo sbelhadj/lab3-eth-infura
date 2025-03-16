b65d7b82e3d7ea331abedb6c09f51281eceadb77
Sami Belhaj <sami.belhadj@gmail.com>
2025-03-16 12:12:05 +0100

    Update sendEth.js

--- scripts/sendEth.js
+++ scripts/sendEth.js
@@ -1,1 +1,19 @@
 
+require("dotenv").config();
+const { ethers } = require("ethers");
+
+const provider = new ethers.JsonRpcProvider(`https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+async function sendTransaction() {
+    const tx = {
+        to: "0xRecipientAddressHere",
+        value: ethers.parseEther("0.01"),
+        gasLimit: "21000",
+    };
+
+    const txResponse = await wallet.sendTransaction(tx);
+    console.log("Transaction Hash:", txResponse.hash);
+}
+
+sendTransaction();

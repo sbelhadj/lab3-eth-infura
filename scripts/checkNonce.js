c2d37409a7d79ce1eeb3f84f89d1717dd6051bf4
Sami Belhaj <sami.belhadj@gmail.com>
2025-03-16 12:12:40 +0100

    Update checkNonce.js

--- scripts/checkNonce.js
+++ scripts/checkNonce.js
@@ -1,1 +1,10 @@
+const { ethers } = require("ethers");
 
+const provider = new ethers.JsonRpcProvider(`https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
+
+async function getNonce(address) {
+    const nonce = await provider.getTransactionCount(address);
+    console.log(`Nonce (Transaction Count) for ${address}: ${nonce}`);
+}
+
+getNonce("0xYourWalletAddressHere");

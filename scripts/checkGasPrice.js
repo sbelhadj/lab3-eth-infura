4ba531b573c26382d8b2a0ba63d8352183c6a7ba
Sami Belhaj <sami.belhadj@gmail.com>
2025-03-16 12:11:41 +0100

    Update checkGasPrice.js

--- scripts/checkGasPrice.js
+++ scripts/checkGasPrice.js
@@ -1,1 +1,10 @@
+const { ethers } = require("ethers");
 
+const provider = new ethers.JsonRpcProvider(`https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
+
+async function getGasPrice() {
+    const gasPrice = await provider.getGasPrice();
+    console.log(`Current Gas Price: ${ethers.formatUnits(gasPrice, "gwei")} Gwei`);
+}
+
+getGasPrice();
